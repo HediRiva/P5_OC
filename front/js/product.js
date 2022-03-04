faeef2dbf3158971bca710428513ebf7c2d78a9c
HediRiva <Hedi.rivas@gmail.com>
2022-03-04 22:11:54 +0100

    fetch et mise en place de tout les éléments sauf les couleurs des canapés dans la liste select

--- front/js/product.js
+++ front/js/product.js
@@ -8,15 +8,30 @@ console.log(productParams);
 const productId = productParams.get('id');
 console.log(productId);
 
-// let productData = [];
-// console.log(productData);
+let productData = [];
+console.log(productData);
 
-fetch(`http://localhost:3000/api/products/${productStringId}`)
-  .then((res) => res.json())
-  .then((promise) => {
-    console.log(promise);
-  })
-  .catch(function (error) {
-    console.log('erreur');
-    return error;
-  });
+async function fillProduct() {
+  let result = await fetch(`http://localhost:3000/api/products/${productId}`);
+  let productData = await result.json();
+  console.log(productData);
+  document.querySelector(
+    '.item__img'
+  ).innerHTML = `<img src="${productData.imageUrl}" alt="${productData.altTxt}" ></img>`;
+  document.querySelector(
+    '#title'
+  ).innerHTML = `<h1 id="title">${productData.name}</h1>`;
+  document.querySelector(
+    '#price'
+  ).innerHTML = `<span id="price">${productData.price}</span>`;
+  document.querySelector(
+    '#description'
+  ).innerHTML = `<p id="description">${productData.description}</p>`;
+  //   let productColor;
+  //   productColor.classList.add('value__color');
+  //   document.querySelector(
+  //     '.value__color'
+  //   ).innerHTML = `<option value="vert">${productData[0]}</option>`;
+}
+
+fillProduct();
